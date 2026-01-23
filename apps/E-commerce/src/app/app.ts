06ef80513a00871836a6a11d222a92dd7cd696c4
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-01-23 16:43:58 +0200

    add lucide icons & remove fortawesome

--- apps/E-commerce/src/app/app.ts
+++ apps/E-commerce/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LucideAngularModule, FileIcon } from 'lucide-angular';
 
 @Component({
-  imports: [ RouterModule],
+  imports: [RouterModule, LucideAngularModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'E-commerce';
+  readonly FileIcon = FileIcon;
 }
