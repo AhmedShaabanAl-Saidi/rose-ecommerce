1d9848ffbdedb80853daaaac91a6dc8ad1265da1
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-02-11 23:46:35 +0200

    add forgot-password form & update auth-title

--- apps/E-commerce/src/app/feature/auth/pages/forgot-password/forgot-password.component.ts
+++ apps/E-commerce/src/app/feature/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,15 +1,54 @@
-import { Component, signal } from '@angular/core';
-import { AuthPage } from '../../../../core/layout/auth-layout/interfaces/auth-page-data';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AuthRepo } from '@elevate/auth-domain';
+import { AuthPage, AuthPageData } from '../../../../core/layout/auth-layout/interfaces/auth-page-data';
+import { UiButtonComponent } from '../../../../shared/components/ui/button/button.component';
+import { TextInputComponent } from '@elevate/reusable-input';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ReactiveFormsModule, UiButtonComponent, TextInputComponent],
   templateUrl: './forgot-password.component.html',
 })
 export class ForgotPasswordComponent implements AuthPage {
-  readonly authData = signal({
+  private readonly auth = inject(AuthRepo);
+  private readonly router = inject(Router);
+  private readonly destroyRef = inject(DestroyRef);
+
+  readonly authData = signal<AuthPageData>({
+    title: 'AUTH.FORGOT_PASSWORD.TITLE',
+    description: 'AUTH.FORGOT_PASSWORD.DESCRIPTION',
     footerText: 'AUTH.FORGOT_PASSWORD.FOOTER_TEXT',
     footerLinkText: 'AUTH.FORGOT_PASSWORD.FOOTER_LINK',
-    footerLinkRoute: '/auth/login'
+    footerLinkRoute: '/auth/login',
+    titleStyle: 'simple'
   });
+
+  readonly isLoading = signal<boolean>(false);
+
+  readonly forgotPasswordForm = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required, Validators.email], nonNullable: true }),
+  });
+
+  submit(): void {
+    if (this.forgotPasswordForm.invalid) {
+      this.forgotPasswordForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading.set(true);
+
+    const payload = this.forgotPasswordForm.getRawValue();
+
+    this.auth.forgetPassword(payload)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.isLoading.set(false);
+          this.router.navigate(['/auth/login']);
+        }
+      });
+  }
 }
