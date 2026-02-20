c030aa2054b7d3790c588b7fb47e23a2314a0d9d
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-02-20 20:12:05 +0200

    use languageService in auth layout

--- apps/E-commerce/src/app/core/layout/auth-layout/components/language-switcher/language-switcher.component.ts
+++ apps/E-commerce/src/app/core/layout/auth-layout/components/language-switcher/language-switcher.component.ts
@@ -1,19 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { languageService } from '../../../../services/language-service';
 
 @Component({
     selector: 'app-language-switcher',
-    imports: [],
     templateUrl: './language-switcher.component.html',
 })
 export class LanguageSwitcherComponent {
-    private readonly translate = inject(TranslateService);
-    currentLang = this.translate.getFallbackLang() || 'en';
+    private readonly langService = inject(languageService);
+
+    readonly currentLang = this.langService.currentLang;
+    readonly isRTL = this.langService.isRTL;
 
     changeLanguage() {
-        const newLang = this.currentLang === 'en' ? 'ar' : 'en';
-        this.translate.use(newLang);
-        this.currentLang = newLang;
-        document.dir = newLang === 'ar' ? 'rtl' : 'ltr';
+        const newLang = this.currentLang() === 'en' ? 'ar' : 'en';
+        this.langService.changeLanguage(newLang);
     }
 }
