f1ab66feb9edffd2634df3796d4acd0f84cf39f4
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-02-20 20:47:17 +0200

    remove unused imports and translation logic from app component

--- apps/E-commerce/src/app/app.ts
+++ apps/E-commerce/src/app/app.ts
@@ -1,14 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LucideAngularModule, FileIcon } from 'lucide-angular';
-import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   imports: [
     RouterModule,
-    LucideAngularModule,
-    TranslateModule,
     NgxSpinnerComponent,
   ],
   selector: 'app-root',
@@ -16,22 +12,5 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   styleUrl: './app.css',
 })
 export class App {
-  private readonly translate = inject(TranslateService);
   protected title = 'E-commerce';
-  readonly FileIcon = FileIcon;
-
-  constructor() {
-    this.translate.addLangs(['en', 'ar']);
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-  }
-
-  changeClient(lang: string) {
-    this.translate.use(lang);
-    if (lang === 'ar') {
-      document.dir = 'rtl';
-    } else {
-      document.dir = 'ltr';
-    }
-  }
 }
