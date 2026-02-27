d1c1e50f298c26781fd624ca2e53f6b0d1b9961c
ibrahimzaher <hemazaher15@gmail.com>
2026-02-27 21:48:30 +0200

    feat: add TrustedBy component with localization support and styling

--- apps/E-commerce/src/app/feature/home/home.ts
+++ apps/E-commerce/src/app/feature/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TrustedByComponent } from './components/trusted/trusted-by.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TrustedByComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
