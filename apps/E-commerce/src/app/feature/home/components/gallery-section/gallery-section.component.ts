a5b50a34b9c5a2959c2cffccc86b9819559914f5
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2026-02-28 21:30:50 +0200

    feat: add arabic and english translations to gallery section

--- apps/E-commerce/src/app/feature/home/components/gallery-section/gallery-section.component.ts
+++ apps/E-commerce/src/app/feature/home/components/gallery-section/gallery-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-gallery-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './gallery-section.component.html',
   styleUrl: './gallery-section.component.css'
 })
