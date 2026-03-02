048357e21c96fdb62bde5edcf78ba1880d84a9f5
VNET\msamy5 <mahmoudsami11095@gmail.com>
2026-03-02 23:31:51 +0200

    Refactor: use reusable heading title component in gallery section
    Change-Id: Ied2c2d94b255493e9ada37eeb0e3ee02c997623e

--- apps/E-commerce/src/app/feature/home/components/gallery-section/gallery-section.component.ts
+++ apps/E-commerce/src/app/feature/home/components/gallery-section/gallery-section.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeadingTitleComponent } from '../../../../shared/components/ui/heading/heading-title.component';
 
 @Component({
   selector: 'app-gallery-section',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, HeadingTitleComponent],
   templateUrl: './gallery-section.component.html',
   styleUrl: './gallery-section.component.css'
 })
