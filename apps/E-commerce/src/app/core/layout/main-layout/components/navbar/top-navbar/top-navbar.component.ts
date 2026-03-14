76e40fbdb8b911f41946a3c0ce6ece90b0f5ee74
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-03-14 09:41:54 +0200

    refactor: add account menu translations and update top navbar to use them

--- apps/E-commerce/src/app/core/layout/main-layout/components/navbar/top-navbar/top-navbar.component.ts
+++ apps/E-commerce/src/app/core/layout/main-layout/components/navbar/top-navbar/top-navbar.component.ts
@@ -2,13 +2,15 @@ import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthRepo, AuthState } from '@elevate/auth-domain';
 import { TextInputComponent } from '@elevate/reusable-input';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { MenuItem } from 'primeng/api';
 import { DividerModule } from 'primeng/divider';
 import { MenuModule } from 'primeng/menu';
+import { languageService } from '../../../../../services/language-service';
 import { LanguageSwitcherComponent } from '../../../../auth-layout/components/language-switcher/language-switcher.component';
 import { ThemeSwitcherComponent } from '../../../../auth-layout/components/theme-switcher/theme-switcher.component';
+
 @Component({
   selector: 'app-top-navbar',
   imports: [
@@ -26,22 +28,44 @@ import { ThemeSwitcherComponent } from '../../../../auth-layout/components/theme
 })
 export class TopNavbarComponent {
   private readonly authState = inject(AuthState);
-  user = this.authState.currentUser;
   private readonly authRepo = inject(AuthRepo);
+  private readonly translate = inject(TranslateService);
+  private readonly language = inject(languageService);
+
+  user = this.authState.currentUser;
 
   items = computed<MenuItem[]>(() => {
+    this.language.currentLang();
+
     const user = this.user();
     if (!user) return [];
+
     return [
       {
         label: user.firstName,
         items: [
-          { label: 'My Profile', icon: 'pi pi-user', routerLink: '/' },
-          { label: 'My Addresses', icon: 'pi pi-map-marker', routerLink: '/' },
-          { label: 'My Orders', icon: 'pi pi-shopping-cart', routerLink: '/' },
-          { label: 'Dashboard', icon: 'pi pi-chart-bar', routerLink: '/' },
           {
-            label: 'Log out',
+            label: this.translate.instant('NAVBAR.ACCOUNT_MENU.PROFILE'),
+            icon: 'pi pi-user',
+            routerLink: '/',
+          },
+          {
+            label: this.translate.instant('NAVBAR.ACCOUNT_MENU.ADDRESSES'),
+            icon: 'pi pi-map-marker',
+            routerLink: '/',
+          },
+          {
+            label: this.translate.instant('NAVBAR.ACCOUNT_MENU.ORDERS'),
+            icon: 'pi pi-shopping-cart',
+            routerLink: '/',
+          },
+          {
+            label: this.translate.instant('NAVBAR.ACCOUNT_MENU.DASHBOARD'),
+            icon: 'pi pi-chart-bar',
+            routerLink: '/',
+          },
+          {
+            label: this.translate.instant('NAVBAR.ACCOUNT_MENU.LOGOUT'),
             icon: 'pi pi-sign-out',
             command: () => this.authRepo.logout().subscribe(),
           },
