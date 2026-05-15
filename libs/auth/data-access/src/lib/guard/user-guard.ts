e0ea59f3873b8e2114248cb7c82b007138d2150c
ibrahimzaher <hemazaher15@gmail.com>
2026-05-15 21:41:41 +0300

    fix(guard): update userGuard to redirect admin users to dashboard instead of unauthorized page

--- libs/auth/data-access/src/lib/guard/user-guard.ts
+++ libs/auth/data-access/src/lib/guard/user-guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { eAuthStateService as AuthStateService } from '@elevate/auth-domain';
 
-export const userGuard: CanActivateFn = (route, state) => {
+export const userGuard: CanActivateFn = () => {
   const router = inject(Router);
   const authState = inject(AuthStateService);
 
   const user = authState.currentUser();
   if (user?.role === 'admin') {
-    router.navigate(['/unauthorized'], { replaceUrl: true });
+    router.navigate(['/dashboard'], { replaceUrl: true });
     return false;
   }
 
