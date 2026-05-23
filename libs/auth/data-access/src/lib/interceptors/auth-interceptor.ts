4a80ee67c22ad5b9cce76e16bd9efbe9826c1507
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2026-05-23 16:21:54 +0300

    bypass token overwritten by interceptor

--- libs/auth/data-access/src/lib/interceptors/auth-interceptor.ts
+++ libs/auth/data-access/src/lib/interceptors/auth-interceptor.ts
@@ -4,6 +4,9 @@ import { AuthStorageService } from '../services/auth-storage.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const storage = inject(AuthStorageService);
+  if (req.url.includes('statistics')) {
+    return next(req);
+  }
   const token = storage.getToken();
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
